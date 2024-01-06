03a9ccca3bd9d59b671ff0398aef4ac206437888
Rimowr <rimwor@outlook.com>
2024-01-06 18:33:46 +0100

    Update responsive styles for text and image blocks

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-/*
- We have set a jQuery wrapper for you to write any JS code -- however you are free to use any framework or library you want. Hint: to accomplish the responsive requriement, a bit of JS coding might be needed.
-*/
-
 (function($) {
   function adjustTextBlockHeight() {
     var imageHeight = $('.image-container img').height();
-    $('.text-block').height(imageHeight - 40);
+    $('.text-block').height(imageHeight);
   }
 
   $(window).on('load resize', function() {
